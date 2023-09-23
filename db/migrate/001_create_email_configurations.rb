414c5e6038ab212c7c9d256f97be0f7fccf7a348
NamTrinh <giangn27@hotmail.com>
2023-09-23 18:42:20 +0700

    Update DB
    Upgrade DB to Redmine v5.0.5

--- db/migrate/001_create_email_configurations.rb
+++ db/migrate/001_create_email_configurations.rb
@@ -1,37 +1,39 @@
 # Create EMAIL_CONFIGURATIONS model
-class CreateEmailConfigurations < ActiveRecord::Migration
-  def change
-    create_table :email_configurations do |t|
-      t.string :configuration_type, null: false
+# Upgrade to Redmine v5.0.5
 
-      t.string :host, null: false
-      t.integer :port, null: false
-      t.boolean :ssl, default: true, null: false
-      t.string :username, null: false
-      t.string :password, null: false
-      t.string :folder, default: 'INBOX', null: false
+class CreateEmailConfigurations < Rails.version < '5.1' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
+  def self.up
+    create_table :email_configurations, if_not_exists: true do |t|
+      t.string 		:configuration_type, null: false
 
-      t.string :move_on_success # IMAP
-      t.string :move_on_failure # IMAP
-      t.boolean :delete_unprocessed, default: false, null: false # POP3
-      t.boolean :apop, default: false, null: false # POP3
+      t.string 		:host, null: false
+      t.integer 	:port, null: false
+      t.boolean 	:ssl, default: true, null: false
+      t.string 		:username, null: false
+      t.string 		:password, null: false
+      t.string 		:folder, default: 'INBOX', null: false
 
-      t.string :unknown_user, default: 'accept', null: false
-      t.boolean :no_account_notice, default: false, null: false
-      t.boolean :no_permission_check, default: true, null: false
-      t.string :default_group
+      t.string 		:move_on_success # IMAP
+      t.string 		:move_on_failure # IMAP
+      t.boolean 	:delete_unprocessed, default: false, null: false # POP3
+      t.boolean 	:apop, default: false, null: false # POP3
 
-      t.references :project, index: false, null: false
-      t.references :tracker, index: false, null: false
-      t.string :category
-      t.string :priority
-      t.string :allow_override
+      t.string 		:unknown_user, default: 'accept', null: false
+      t.boolean 	:no_account_notice, default: false, null: false
+      t.boolean 	:no_permission_check, default: true, null: false
+      t.string 		:default_group
 
-      t.datetime :last_fetch_at, null: true
-      t.boolean :flg_active, default: true
+      t.references 	:project, index: false, null: false
+      t.references 	:tracker, index: false, null: false
+      t.string 		:category
+      t.string 		:priority
+      t.string 		:allow_override
+
+      t.datetime 	:last_fetch_at, null: true
+      t.boolean 	:flg_active, default: true
     end
 
-    add_index(:email_configurations, [:host, :port, :username, :folder],
-              unique: true, name: 'index_unique_email_configuration')
+	add_index :email_configurations, [:host, :port, :username, :folder], unique: true, name: 'index_unique_email_configuration', if_not_exists: true
+
   end
 end
